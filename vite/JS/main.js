f03c7449775d5090d74e5000e5e3febc1c5e21a2
Judy <158211720+judyyan27@users.noreply.github.com>
2024-11-06 12:27:03 -0500

    work on card

--- vite/JS/main.js
+++ vite/JS/main.js
@@ -1,1 +1,26 @@
 import "../CSS/style.css";
+import { flowers } from "./flowers.js";
+
+const DOMSelectors = {
+  form: document.querySelector(`form`),
+  button: document.querySelector(`.submit`),
+  container: document.querySelector(`#container-box`),
+  name: document.querySelector(`#name`),
+  flavor: document.querySelector(`#description`),
+};
+
+DOMSelectors.button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  DOMSelectors.container.insertAdjacentHTML("beforeend");
+  `    <div class="card">
+      <h3 class="card-title">${title}</h1>
+      <h4 class="card-desc">${description}</p>
+      <h5 class="card-price">${price}</h2>
+      <img
+        class="card-img"
+        src="${image}"
+        alt="${altText}"
+      />
+    </div>`;
+});
